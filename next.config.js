3765c4ca6e03069fb7e2c25a90635ce16c78b71f
Edith <ebalbouze@medine.dev>
2022-09-30 14:59:28 -0400

    COnfiguracion de PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-module.exports = {
+const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+module.exports = withPWA({
+  pwa: {
+    register: true,
+    mode: 'production',
+    disable: process.env.NODE_ENV !== 'production',
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/],
+  },
   reactStrictMode: true,
   images: {
     domains: ['api.lorem.space', 'placeimg.com', 'img-mobile.rubias19.com', 'rickandmortyapi.com', 'ar.celulares.com', 'images.philips.com'],
   },
-}
+});
